fc4f1f781704090ffd8e2d632319fb9141226588
sridaopa3 <sridaopa@uoc.edu>
2026-04-27 15:54:00 +0200

    Adaptación almacenaje.js + resolver.js + schema.js

--- js/almacenaje.js
+++ js/almacenaje.js
@@ -1,43 +1,45 @@
-const CLAVE_USUARIOS = "usuarios";
-const CLAVE_SESION = "usuarioLogueado";
+/**const CLAVE_USUARIOS = "usuarios";
+const CLAVE_SESION = "usuarioLogueado";*/
+
+//Declaramos variables
+let ofertas = [
+    {id:"1", titulo:"Tractorista", empresa:"Campo Grande",ubicacion:"Tórrec", descripcion:"Manejo de maquinaria agrícola y apoyo en tareas de campo.", fecha:"27/03/2026"},
+    {id: "2", titulo: "Ingeniero agrónomo", empresa: "Monmalo", ubicacion: "Agramunt", descripcion: "Gestión de fincas, mejora de producción y coordinación técnica.", fecha: "25/03/2026"}
+];
+
+let demandas = [
+    {id: "3", nombre: "Laura Gómez", profesion: "Marketing Digital", disponibilidad: "Inmediata", descripcion: "Interesada en comunicación digital, redes sociales y campañas online.", fecha: "26/03/2026"},
+    {id: "4", nombre: "Carlos Pérez", profesion: "Soporte IT", disponibilidad: "15 días", descripcion: "Experiencia en soporte técnico, incidencias y atención a usuarios.", fecha: "27/03/2026"}
+];
+
+let usuarios = [
+    {id: "1", nombre: "Admin", email: "admin@agrojobs.com", password: "123456aA", rol: "Administrador"},
+    {id: "2", nombre: "Laura Gómez", email: "laura@correo.com", password: "123456aA", rol: "Candidato"},
+    {id: "3", nombre: "Campo Grande", email: "rrhh@campogrande.com", password: "123456aA", rol: "Empresa"}
+];
 
 // --- MÓDULO CRUD Y STORAGE ---
 export const Almacenaje = {
-    obtenerUsuarios: () => {
-        const data = localStorage.getItem(CLAVE_USUARIOS);
-        return data ? JSON.parse(data) : [];
-    },
-
-    guardarUsuarios: (usuarios) => {
-        localStorage.setItem(CLAVE_USUARIOS, JSON.stringify(usuarios));
-    },
-
-    buscarUsuario: (email) => {
-        const usuarios = Almacenaje.obtenerUsuarios();
-        return usuarios.find(u => u.email === email);
-    },
-
-    borrarUsuario: (email) => {
-        const usuarios = Almacenaje.obtenerUsuarios();
-        const filtrados = usuarios.filter(u => u.email !== email);
-        Almacenaje.guardarUsuarios(filtrados);
-    },
+    obtenerUsuarios: () => usuarios,
+    guardarUsuarios: (nuevosUsuarios) => {usuarios=nuevosUsuarios;},
+    buscarUsuario: (email) => {return usuarios.find(u => u.email === email);},
+    borrarUsuario: (email) => {usuarios = usuarios.filter(u => u.email !== email);},
 
     // --- DATOS DE SESION ---
-    getSesion: () => localStorage.getItem(CLAVE_SESION),
+    /**getSesion: () => localStorage.getItem(CLAVE_SESION),
     setSesion: (email) => localStorage.setItem(CLAVE_SESION, email),
-    borrarSesion: () => localStorage.removeItem(CLAVE_SESION),
+    borrarSesion: () => localStorage.removeItem(CLAVE_SESION),*/
 
-    // --- DATOS DE OFERTAS ---
-    obtenerOfertas: () => JSON.parse(localStorage.getItem("ofertas") || "[]"),
-    guardarOfertas: (ofertas) => localStorage.setItem("ofertas", JSON.stringify(ofertas)),
-    obtenerDemandas: () => JSON.parse(localStorage.getItem("demandas") || "[]"),
-    guardarDemandas: (demandas) => localStorage.setItem("demandas", JSON.stringify(demandas)),
+    // --- DATOS DE OFERTAS Y DEMANDAS ---
+    obtenerOfertas: () => ofertas,
+    guardarOfertas: (nuevasOfertas) => {ofertas=nuevasOfertas;},
+
+    obtenerDemandas: () => demandas,
+    guardarDemandas: (nuevasDemandas) => {demandas=nuevasDemandas;},
 };
 
-
 // --- LÓGICA DE INTERFAZ REUTILIZABLE --- (Evitamos repetir funciones en todos los archivos js)
-export function actualizarNavbar() {
+/**export function actualizarNavbar() {
     const emailLogueado = Almacenaje.getSesion();
     const zonaSesion = document.getElementById("zona-sesion");
 
@@ -65,4 +67,4 @@ export function actualizarNavbar() {
 export function cerrarSesion() {
     Almacenaje.borrarSesion(); 
     window.location.href = "index.html"; 
-}
+}*/
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,59 +1,33 @@
-let ofertas = [
-    {id:"1", titulo:"Tractorista", empresa:"Campo Grande",ubicacion:"Tórrec", descripcion:"Manejo de maquinaria agrícola y apoyo en tareas de campo.", fecha:"27/03/2026"},
-    {id: "2", titulo: "Ingeniero agrónomo", empresa: "Monmalo", ubicacion: "Agramunt", descripcion: "Gestión de fincas, mejora de producción y coordinación técnica.", fecha: "25/03/2026"}
-];
-
-let demandas = [
-    {id: "3", nombre: "Laura Gómez", profesion: "Marketing Digital", disponibilidad: "Inmediata", descripcion: "Interesada en comunicación digital, redes sociales y campañas online.", fecha: "26/03/2026"},
-    {id: "4", nombre: "Carlos Pérez", profesion: "Soporte IT", disponibilidad: "15 días", descripcion: "Experiencia en soporte técnico, incidencias y atención a usuarios.", fecha: "27/03/2026"}
-];
-
-let usuarios = [
-    {id: "1", nombre: "Admin", email: "admin@agrojobs.com", password: "123456aA", rol: "Administrador"},
-    {id: "2", nombre: "Laura Gómez", email: "laura@correo.com", password: "123456aA", rol: "Candidato"},
-    {id: "3", nombre: "Campo Grande", email: "rrhh@campogrande.com", password: "123456aA", rol: "Empresa"}
-];
+//Importaciones librerias
+import { Almacenaje } from "../../js/almacenaje.js";
 
 export const root = {
-    getOfertas: () => ofertas,
-    getDemandas: () => demandas,
-    getUsuarios: () => usuarios,
+    getOfertas: () => Almacenaje.obtenerOfertas(),
+    getDemandas: () => Almacenaje.obtenerDemandas(),
+    getUsuarios: () => Almacenaje.obtenerUsuarios(),
 
-    crearOferta: (args) => {
-        const nuevaOferta = { 
-            id: String(ofertas.length + 1), 
-            titulo: args.titulo,
-            empresa: args.empresa,
-            ubicacion: args.ubicacion,
-            descripcion: args.descripcion,
-            fecha: args.fecha
-        };
-        ofertas.push(nuevaOferta);
-        return nuevaOferta;
-    },
+    crearOferta: ({titulo,empresa,ubicacion,descripcion,fecha}) => {
+        const lista=Almacenaje.obtenerOfertas();
+        const nueva={id:String(lista.length+1),titulo,empresa,ubicacion,descripcion,fecha};
+        lista.push(nueva);
+        Almacenaje.guardarOfertas(lista);
+        return nueva;
+    },   
 
-    crearDemanda: (args) => {
-        const nuevaDemanda = {
-            id: String(demandas.length + 1),
-            nombre: args.nombre,
-            profesion: args.profesion,
-            disponibilidad: args.disponibilidad,
-            descripcion: args.descripcion,
-            fecha: args.fecha
-        };
-        demandas.push(nuevaDemanda);
-        return nuevaDemanda;
-    },
+    crearDemanda: ({nombre,profesion,disponibilidad,descripcion,fecha}) =>{
+        const lista=Almacenaje.obtenerDemandas();
+        const nueva={id:String(lista.length+1),nombre,profesion,disponibilidad,descripcion,fecha};
+          lista.push(nueva);
+          Almacenaje.guardarDemandas(lista);
+          return nueva;
+    }, 
 
-    crearUsuario: (args) => {
-        const nuevoUsuario = {
-            id: String(usuarios.length + 1),
-            nombre: args.nombre,
-            email: args.email,
-            password: args.password,
-            rol: args.rol
-        };
-        usuarios.push(nuevoUsuario);
-        return nuevoUsuario;
+    crearUsuario: ({nombre,email,password,rol}) => {
+        const lista = Almacenaje.obtenerUsuarios();
+        const nuevo ={id:String(lista.length+1), nombre,email,password,rol};
+        lista.push(nuevo);
+        Almacenaje.guardarUsuarios(lista);
+        return nuevo;
     }
+
 };
